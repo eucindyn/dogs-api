1b3b9403a121b37c6456b85425a92690d3e8040d
eucindyn <cindy.nsc1@gmail.com>
2022-12-24 15:47:40 -0300

    criar tamanho de foto automático

--- functions.php
+++ functions.php
@@ -11,6 +11,10 @@ require_once $dirbase . "/endpoints/photo_post.php";
 //require_once $dirbase . "/endpoints/photo_get.php";
 //require_once $dirbase . "/endpoints/photo_delete.php";
 
+update_option("large_size_w", 1000);
+update_option("large_size_h", 1000);
+update_option("large_crop", 1);
+
 function change_api($slug) {
   return "json";
 }
