8bd272dbf0159a1ecd358aef1ba708eeb491485a
eucindyn <cindy.nsc1@gmail.com>
2022-12-24 16:39:58 -0300

    resetar senha

--- endpoints/password.php
+++ endpoints/password.php
@@ -43,35 +43,35 @@ add_action("rest_api_init", "register_api_password_lost");
 
 // Password Reset
 
-// function api_password_reset($request) {
-//   $login = $request["login"];
-//   $password = $request["password"];
-//   $key = $request["key"];
-//   $user = get_user_by("login", $login);
+function api_password_reset($request) {
+  $login = $request["login"];
+  $password = $request["password"];
+  $key = $request["key"];
+  $user = get_user_by("login", $login);
 
-//   if(empty($user)) {
-//     $response = new WP_Error("error", "this user doesn't exist", ["status" => 401]);
-//     return rest_ensure_response($response);
-//   }
+  if(empty($user)) {
+    $response = new WP_Error("error", "this user doesn't exist", ["status" => 401]);
+    return rest_ensure_response($response);
+  }
 
-//   $check_key = check_password_reset_key($key, $login);
+  $check_key = check_password_reset_key($key, $login);
 
-//   if(is_wp_error($check_key)) {
-//     $response = new WP_Error("error", "the token has expired", ["status" => 401]);
-//     return rest_ensure_response($response);
-//   }
+  if(is_wp_error($check_key)) {
+    $response = new WP_Error("error", "expired token", ["status" => 401]);
+    return rest_ensure_response($response);
+  }
 
-//   reset_password($user, $password);
+  reset_password($user, $password);
 
-//   return rest_ensure_response("password has been changed");
-// }
+  return rest_ensure_response("password has been changed");
+}
 
-// function register_api_password_reset() {
-//   register_rest_route("api", "/password/reset", [
-//     "methods" => WP_REST_Server::CREATABLE,
-//     "callback" => "api_password_reset",
-//   ]);
-// }
-// add_action("rest_api_init", "register_api_password_reset");
+function register_api_password_reset() {
+  register_rest_route("api", "/password/reset", [
+    "methods" => WP_REST_Server::CREATABLE,
+    "callback" => "api_password_reset",
+  ]);
+}
+add_action("rest_api_init", "register_api_password_reset");
 
-// ?>
+?>
