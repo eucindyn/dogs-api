d3d7a919da82e5cc31c8db321141e44b87793c62
eucindyn <cindy.nsc1@gmail.com>
2022-12-24 15:40:27 -0300

    postar foto

--- functions.php
+++ functions.php
@@ -1,11 +1,15 @@
 <?php
 
-remove_action("rest_api_init", "create_initial_rest_routes", 99);
+//remove_action("rest_api_init", "create_initial_rest_routes", 99);
 
 $dirbase = get_template_directory();
 
 require_once $dirbase . "/endpoints/user_post.php";
 require_once $dirbase . "/endpoints/user_get.php";
+
+require_once $dirbase . "/endpoints/photo_post.php";
+//require_once $dirbase . "/endpoints/photo_get.php";
+//require_once $dirbase . "/endpoints/photo_delete.php";
 
 function change_api($slug) {
   return "json";
