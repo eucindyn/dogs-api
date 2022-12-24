7a75c77127e372d5e00e494f146435b13f16d554
eucindyn <cindy.nsc1@gmail.com>
2022-12-24 14:25:57 -0300

    criar pasta com endpoints

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 remove_action("rest_api_init", "create_initial_rest_routes", 99);
 
+$dirbase = get_template_directory();
 require_once $dirbase . "/endpoints/user_post.php";
 
 function change_api($slug) {
