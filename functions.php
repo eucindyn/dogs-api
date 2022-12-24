c34bb6c55f091564bef22847d6d7e458f6a6e205
eucindyn <cindy.nsc1@gmail.com>
2022-12-24 16:49:44 -0300

    criar uma lista de estatísticas para usuário

--- functions.php
+++ functions.php
@@ -21,6 +21,8 @@ require_once $dirbase . "/endpoints/photo_delete.php";
 require_once $dirbase . "/endpoints/comment_post.php";
 require_once $dirbase . "/endpoints/comment_get.php";
 
+require_once $dirbase . "/endpoints/stats_get.php";
+
 require_once $dirbase . "/endpoints/password.php";
 
 update_option("large_size_w", 1000);
